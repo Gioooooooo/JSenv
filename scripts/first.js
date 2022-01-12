b459ee34c2162797fc45ebad70741c89eb7c05dd
Gioooooooo <kuprashviligiorgi3@gmail.com>
2022-01-12 05:26:42 -0800

    adding all files

---
+++ scripts/first.js
@@ -0,0 +1,4 @@
+console.log("1st");
+
+let x = 2;
+console.log(x);
---
+++ src/index.js
@@ -0,0 +1,6 @@
+console.log("file: index.js");
+
+import { sayHi, sayBye } from "./index2.js";
+
+sayHi("Kupra");
+sayBye("Kupra");
